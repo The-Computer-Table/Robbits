a1a964231ee91beb7119c2f0d184756b91a9a18f
The-Computer-Table <104786578+The-Computer-Table@users.noreply.github.com>
2022-05-24 13:33:09 -0400

    Add files via upload

---
+++ map/robbitimgtoarray.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import sys; name=sys.argv[1]
+#name=input("Input File Name: ")
+img=Image.open(f"{name}.png").convert('RGB')
+w,h=img.size
+def RGBtoTILEID(x):
+    tileid={
+    (255,255,255):0, #white blank
+    (195,195,195):1, #light grey slowing
+    (255,127,39):2, #orange lava
+    (185,122,86):3, #brown crate
+    (136,0,27):4,#darkred blank+spawn
+    (63,72,204):5,#indigo teleporter
+    (0,0,0):6 #black -wall
+    }
+    return tileid[x] if x in tileid else 0
+f=open(f"{name}.json","w")
+f.write(str(([[RGBtoTILEID(img.getpixel((i,j)) )for i in range(w)] for j in range(h)])))
+#0-blank
+#1-slowing
+#2-lava
+#3-crate
+#4-enemy spawn
+#5 teleporter
